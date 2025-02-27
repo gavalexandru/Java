b85553becca4c7ef3a41d29cfabef086cd4f5361
gavril_alexandru <80162292+gavalexandru@users.noreply.github.com>
2025-02-27 10:43:18 +0200

    Lab 1 - Compulsory

---
+++ Lab 1/Compulsory /Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        int n = (int)(Math.random() * 1_000_000);
+        n = n*3;
+        n = n+Integer.parseInt("10101",2);
+        n = n+Integer.parseInt("FF",16);
+        n = n*6;
+        int ok=0;
+        while(ok!=1){
+            if(n<10) ok=1;
+            else {
+            int aux=0;
+            while(n!=0){
+                aux+=n%10;
+                n=n/10;
+            }
+            n=aux;
+            }
+        }
+        System.out.println("Willy-nilly, this semester I will learn " + languages[n]);
+    }
+}
