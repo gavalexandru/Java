e8bdcae27bef9786f8bd61076d2f1c7dded4789a
gavril_alexandru <80162292+gavalexandru@users.noreply.github.com>
2025-03-06 08:59:15 +0200

    Create Main.java

---
+++ Lab2/Compulsory/Main.java
@@ -0,0 +1,12 @@
+package Compulsory;
+/**
+ * Autor: Alexandru Gavril
+ */
+public class Main{
+public static void main(String[] args) {
+      Student student1 = new Student("Alex");
+      Project project1 = new Project("Programare avansata", projectType.theoretical);
+      System.out.println(student1);
+      System.out.println(project1);
+    }
+}
