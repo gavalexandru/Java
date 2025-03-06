b444f59454d006493771e163949a75045b54a779
gavril_alexandru <80162292+gavalexandru@users.noreply.github.com>
2025-03-06 09:00:20 +0200

    Create Student.java

---
+++ Lab2/Compulsory/Student.java
@@ -0,0 +1,20 @@
+package Compulsory;
+/**
+ * Autor: Alexandru Gavril
+ */
+public class Student {
+    private String name;
+    public Student(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+     return "Numele studentului este: " + name;
+    }
+}
